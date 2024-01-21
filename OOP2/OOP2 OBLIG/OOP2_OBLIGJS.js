04df375f5c9c1ca0692b2df6112bb8cf15e7f6cf
sebbur04 <sebaseb@stud.ntnu.no>
2024-01-21 23:12:59 +0100

    OOP2 Ferdig med ekstraoppgaver og forbedringer

--- OOP2/OOP2 OBLIG/OOP2_OBLIGJS.js
+++ OOP2/OOP2 OBLIG/OOP2_OBLIGJS.js
@@ -1,4 +1,32 @@
 //OOP2 Oblig 2 i DCST1007 - Sebastian E Burmo
+console.log("Oppgave 1");
+console.log("Se konsollen for å se resultatet av oppgave 1 med klasser");
+
+//------------------------------------//
+//Automatisere navn, saldo og type konti uten hardkoding - For oppgave 5 ekstra//
+
+document.getElementById('kontoForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  let navn = document.getElementById('navn').value;
+  let saldo = document.getElementById('saldo').value;
+  let type = document.getElementById('type').value;
+  let kundenummer = Math.floor(Math.random() * 10000000) + 1; //Random kundenummer mellom 1 og 10000000 som i systemet, autogenerert med Math.random
+
+  let konto;
+  if (type === 'voksen') {
+    konto = new Konto(kundenummer,navn, saldo);
+  } else {
+    konto = new Barnekonto(kundenummer,navn, saldo);
+  }
+
+  // Print out account information
+  konto.kontoInformasjon();
+});
+
+
+
+//--------------Definere klasser i banken-----------------------//
 
 class Konto {
   constructor(kundenummer, navn, saldo) {
@@ -11,11 +39,14 @@ class Konto {
     this.saldo += amount;
   }
 
+  //Failsafe for at en ikke kan ta ut mer enn det som er på kontoen
   uttak(amount) {
     if (amount <= this.saldo) {
       this.saldo -= amount;
     } else {
-      console.log("Ikke nok penger på kontoen.");
+      console.log(
+        `${this.navn} med kundenummer ${this.kundenummer} har ikke nok penger på kontoen for denne handlingen.`
+        );
     }
   }
 
@@ -27,30 +58,71 @@ class Konto {
 }
 
 class Barnekonto extends Konto {
-  constructor(kundenummer, navn) {
-    super(kundenummer, navn, 200); //Subklasse fra konto som har 200 kroner i saldo fra starten
+  constructor(kundenummer, navn, saldo) {
+    super(kundenummer, navn, saldo + 200); //Subklasse fra konto som har 200 kroner i saldo fra starten + evt saldo de har bygd opp
   }
 }
 
-// Opprett en vanlig konto ved klassenavn Konto
-let kari = new Konto(93827100, "Kari Hansen", 429);
-kari.innskudd(0);
-kari.uttak(0); //Etter innskudd og uttak skal Kari ha 429 kroner på konto
-kari.kontoInformasjon();
+//--------BARNE KONTI--------//
+console.log("Oppgave 2");
+// Opprett en barnekonto ved klassenavn Barnekonto
+let emil = new Barnekonto(93827101, "Emil Barn",0); //Starter på 200 siden den arver super med 200kr fra Barnekonti
+emil.innskudd(0);
+emil.uttak(0);
+emil.kontoInformasjon();
 
 
-let sebastian = new Konto(93827102, "Sebastian Burmo", 895);
-sebastian.innskudd(100+100);
+//--------VOKSEN KONTI--------//
+
+
+console.log("Oppgave 3");
+let sebastian = new Konto(93827102, "Sebastian Burmo Voksen", 895);
+sebastian.innskudd(0);
 sebastian.uttak(200); //
 sebastian.kontoInformasjon(); 
 
 
 
+console.log("Oppgave 4");
+let kari = new Konto(93827100, "Kari Hansen Voksen", 429);
+kari.innskudd(0);
+kari.uttak(0); //Etter innskudd og uttak skal Kari ha 429 kroner på konto
+kari.kontoInformasjon();
 
 
-// Opprett en barnekonto ved klassenavn Barnekonto
-let ole = new Barnekonto(93827101, "Ole Olsen"); //Starter på 200 siden den arver super med 200kr fra Barnekonti
-ole.innskudd(0);
-ole.uttak(0);
-ole.kontoInformasjon();
+//-------------- Oppgave 5-----------------------//
 
+console.log("Oppgave 5");
+
+let lise = new Barnekonto(93827103, "Lise Jensen", 0);
+let eva = new Konto(93827104, "Eva Hansen", 895);
+let petter = new Konto(93827105, "Petter Olsen", 0);
+
+// Transaksjoner , simulert en dag med aktivitet i min fiktive bank
+
+console.log("Klokka 10:30 – Eva tar ut 300 kroner.");
+eva.uttak(300);
+eva.kontoInformasjon();
+
+console.log("Klokka 11:00 – Bestefaren til Lise er i det gavmilde hjørnet, og setter inn 4000 kroner.");
+lise.innskudd(4000);
+lise.kontoInformasjon();
+
+console.log("Klokka 11:00 – Petter setter inn 3000 kroner.");
+petter.innskudd(3000);
+petter.kontoInformasjon();
+
+console.log("Klokka 12:15 – Eva overfører 250 kroner i bursdagsgave til Petter.");
+eva.uttak(250);
+petter.innskudd(250);
+eva.kontoInformasjon();
+petter.kontoInformasjon();
+
+console.log("Klokka 17:30 – Eva forsøker å ta ut 800 kroner for å kjøpe kjole til julebordet.");
+eva.uttak(800);
+eva.kontoInformasjon();
+
+
+//--------------Oppgave 6 Ekstraoppgave-----------------------//
+console.log("Oppgave 6");
+console.log("Bruk Forms for å opprette nye konti som vises under her");
